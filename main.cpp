365ab1182162f554e092a424ec6bdff592b00698
UnicellularT <tyc123126@126.com>
2023-04-12 23:49:12 +0800

    again

--- main.cpp
+++ main.cpp
@@ -7,10 +7,11 @@ using namespace std;
 int main()
 {
    vector<string> msg {"Hello", "C++", "World", "from", "VS Code", "and the C++ extension!"};
-    
+    //refresh
    for (const string& word : msg)
    {
       cout << word << " ";
    }
+   int a=1;
    cout << endl;
 }
